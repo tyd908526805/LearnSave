9362d201f9f60921dc8a283bad01b49e5704278d
VM <30572094+tyd908526805@users.noreply.github.com>
2022-12-02 12:08:01 +0800

    优化了时间消耗和内存空间消耗

--- 1.两数之和/twoSum.cpp
+++ 1.两数之和/twoSum.cpp
@@ -1,35 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> result;
-        /* O(n^2)
-        for (int i = 0; i < nums.size(); i++)
-        {
-            for (int j = i + 1; j < nums.size(); j++)
-            {
-                if (nums[j] == target - nums[i])
-                {
-                    result.push_back(i);
-                    result.push_back(j);
-                    return result;
-                }
-            }
-        }
-        return result;
-        */ 
-        // new
         unordered_map<int, int> tmpMap;
         for (int i = 0; i < nums.size(); i++)
         {
             auto iter = tmpMap.find(target - nums[i]);
             if (iter == tmpMap.end())
-                tmpMap.insert(make_pair(nums[i], i));
+                tmpMap.insert(pair(nums[i],i));
             else
-            {
-                result.push_back(iter->second);
-                result.push_back(i);
-            }
+                return {iter->second, i};
         }
-        return result;
+        return {};
     }
 };
